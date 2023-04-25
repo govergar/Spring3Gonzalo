2df90f63c570874b94133247e7b9be7f0299fca5
govergar <govergar@gmail.com>
2023-04-24 22:56:13 -0400

    Ultimo cambio

--- Spring3Gonzalo.py
+++ Spring3Gonzalo.py
@@ -18,32 +18,41 @@ def crear_password():
                 z=str(randint(0,9))
             password=str((password)+i+j+z)
     return  password
-#Función que pregunta y almacena número de teléfono
 
-
+# asignamos a cada cliente una contraseña
 opc=input('Desea crear cuenta a los clientes (Si/No): ')
 # creamos las cuentas de cada usuario con su respectiva contraseña
 if opc=="Si":
     cuentas_password={}
     for nombre in clientes:
+        #creamos un diccionario donde guardamos el nombre y el password de cada cuenta
         cuentas_password[nombre]=crear_password()
         
 else:
     print('Que tenga buen día')
 
-print(cuentas_password)
+# mostramos todos los password de las cuentas
+print("Las contraseñas asociadas a cada cuenta son las siguientes:")
+for k, v in cuentas_password.items():
+    print("la contraseña de "+ k + " = "+ v)
 
+# ahora pedimos que complete las cuentas con un número de telefono valido
+print("Para completar proceso debe ingresar un número de teléfono valido para cada usuario")
+# se crea diccionario para almacenar los número de teléfono
 cuentas_telefono={}
 i=0
 while i<len(clientes):
     aux=input('Porfavor ingrese el número de contacto de '+ clientes[i]+':')
     if len(aux)>=8:
-        print('Número ingresado de forma correcta por favor ingrese el siguiente')
-        cuentas_telefono[clientes[i]]=aux
-        i=i+1
+        if i<9:
+            print('Número ingresado de forma correcta por favor ingrese el siguiente')
+            cuentas_telefono[clientes[i]]=aux
+            i=i+1
+        elif i==9:
+            print("Todos los números telefónicos ingresados de forma correcta, proceso de creación de cuentas terminado....!!!Muchas Gracias!!!!")
+            i=i+1
     else:
         print("El número de contacto debe tener al menos 8 dígitos, intente nuevamente")
         i=i
-print(cuentas_telefono) 
 
-print('version final')
+
